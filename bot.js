9e30cd52283a226b1f21e555a7f801c79d52fd17
MrD7z <39459185+MrD7z@users.noreply.github.com>
2018-08-24 22:43:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const ytdl = require('ytdl-core');
-const request = require('request');
 const fs = require('fs');
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
-const Canvas = require('canvas');
-const jimp = require('jimp');
-const superagent = require('superagent');
 const moment = require("moment");
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
